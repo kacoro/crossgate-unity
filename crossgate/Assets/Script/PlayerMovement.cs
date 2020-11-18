68b0f0c97887fd976268f1a465b682667e5c0cb3
kacoro <kacoro@qq.com>
2020-11-18 16:19:33 +0800

    fixed occlude

--- crossgate/Assets/Script/PlayerMovement.cs
+++ crossgate/Assets/Script/PlayerMovement.cs
@@ -6,7 +6,7 @@ public class PlayerMovement : MonoBehaviour
 {
    private Rigidbody2D rb;
    private float moveH,moveV;
-   [SerializeField] private float moveSpeed = 2.0f;
+   [SerializeField] private float moveSpeed = 64.0f;
 
    private void Start() {
        rb = GetComponent<Rigidbody2D>();
