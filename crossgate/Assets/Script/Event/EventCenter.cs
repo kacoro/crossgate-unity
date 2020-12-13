eee60bb06c1baa9a290aa6d7fcf79b40f083b003
kacoro <kacoro@qq.com>
2020-12-13 16:54:25 +0800

    update framwork

--- crossgate/Assets/Script/Event/EventCenter.cs
+++ crossgate/Assets/Script/Event/EventCenter.cs
@@ -4,6 +4,31 @@ using UnityEngine;
 using UnityEngine.Events;
 
 //事件中心模块（基于观察者设计模式）
+
+// 装箱和拆箱
+
+
+public interface IEventInfo {
+    //这是一个空接口
+}
+
+public class EventInfo<T> : IEventInfo {
+    public UnityAction<T> actions;
+    public EventInfo(UnityAction<T> action) {
+        actions += action;
+    }
+}
+
+public class EventInfo : IEventInfo
+{
+    public UnityAction actions;
+ 
+    public EventInfo(UnityAction action)
+    {
+        actions += action;
+    }
+}
+
 public class EventCenter : BaseManager<EventCenter>
 {
     //字典中，key对应着事件的名字，
@@ -11,43 +36,74 @@ public class EventCenter : BaseManager<EventCenter>
     //添加事件监听
     //第一个参数：事件的名字
     //第二个参数：处理事件的方法
-     private Dictionary<string, UnityAction<object>> eventDic
-        = new Dictionary<string, UnityAction<object>>();
-    public void AddEventListener(string name, UnityAction<object> action) {
+     private Dictionary<string, IEventInfo> eventDic
+        = new Dictionary<string,IEventInfo>();
+    public void AddEventListener<T>(string name, UnityAction<T> action) {
         //有没有对应的事件监听
         //有的情况
         if (eventDic.ContainsKey(name))
         {
-            eventDic[name] += action;
+             (eventDic[name] as EventInfo<T>).actions += action;
         }
         //没有的情况
         else {
-            eventDic.Add(name, action);
+            eventDic.Add(name, new EventInfo<T>(action));
+        }
+    }
+    //对于不需要参数的情况的重载方法
+    public void AddEventListener(string name, UnityAction action)
+    {
+        if (eventDic.ContainsKey(name))
+        {
+            (eventDic[name] as EventInfo).actions += action;
+        }
+        else
+        {
+            eventDic.Add(name, new EventInfo(action));
         }
     }
 
+
     //通过事件名字进行事件触发
-    public void EventTrigger(string name,object info) {
+   public void EventTrigger<T>(string name,T info) {
         //有没有对应的事件监听
         //有的情况（有人关心这个事件）
         if (eventDic.ContainsKey(name))
         {
             //调用委托（依次执行委托中的方法）
-            eventDic[name](info);
+            //？是一个C#的简化操作
+            (eventDic[name] as EventInfo<T>).actions?.Invoke(info);
+        }
+    }
+    //对于不需要参数的情况的重载方法
+    public void EventTrigger(string name)
+    {
+        if (eventDic.ContainsKey(name))
+        {
+            (eventDic[name] as EventInfo).actions?.Invoke();
         }
     }
 
      //移除对应的事件监听
-    public void RemoveEventListener(string name, UnityAction<object> action) {
+     public void RemoveEventListener<T>(string name, UnityAction<T> action) {
         if (eventDic.ContainsKey(name))
         {
             //移除这个委托
-            eventDic[name] -= action;
+            (eventDic[name] as EventInfo<T>).actions -= action;
+        }
+    }
+     //对于不需要参数的情况的重载方法
+    public void RemoveEventListener(string name, UnityAction action)
+    {
+        if (eventDic.ContainsKey(name))
+        {
+            (eventDic[name] as EventInfo).actions -= action;
         }
     }
 
+
     //清空所有事件监听(主要用在切换场景时)
-    public void Clear() {
+     public void Clear() {
         eventDic.Clear();
     }
 }
---
+++ crossgate/Assets/Script/Input/InputTest.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //开启输入检测
+        InputMgr.GetInstance().StartOrEndCheck(true);
+        //添加事件监听
+        EventCenter.GetInstance().AddEventListener<KeyCode>("KeyisDown", CheckInputDown);
+        EventCenter.GetInstance().AddEventListener<KeyCode> ("KeyisUp", CheckInputUp);
+    }
+    private void CheckInputDown(KeyCode obj) {
+        switch (obj) {
+            case KeyCode.W:
+                Debug.Log("前进");
+                break;
+            case KeyCode.A:
+                break;
+            case KeyCode.S:
+                break;
+            case KeyCode.D:
+                break;
+        }
+    }
+    private void CheckInputUp(KeyCode obj) {
+        switch (obj)
+        {
+            case KeyCode.W:
+                Debug.Log("不再前进");
+                break;
+            case KeyCode.A:
+                break;
+            case KeyCode.S:
+                break;
+            case KeyCode.D:
+                break;
+        }
+    }
+    private void OnDestroy()
+    {
+        EventCenter.GetInstance().RemoveEventListener<KeyCode>("KeyisDown", CheckInputDown);
+        EventCenter.GetInstance().RemoveEventListener<KeyCode>("KeyisUp", CheckInputUp);
+    }
+
+}
